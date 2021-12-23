f511e7fbc24993eff816ed8efa02fcdba402e3f5
akkayy <ahmed.kaja@outlook.com>
2021-12-23 23:15:52 +1100

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<p>Welcome to a demo of how styles are being processed by dev servers</p>
+
+    <h4>Dev Servers under scope are:</h4>
+		<ul>
+			<li>- Webpack</li>
+			<li>- Vite</li>
+		</ul>
+	</div>
+</template>
+
+<style data-pg-id="101">
+	@import './assets/base.css';
+	/* @import '@/assets/base.css'; */
+
+  p, .pg-node-id-2 {
+		color: #5add64;
+		padding-bottom: 10px;
+	}
+</style>
+
+<style lang="scss" scoped data-pg-id="102">
+	ul, .pg-node-id-3 {
+		li, .pg-node-id-4 {
+			margin: 10px;
+		}
+	}
+</style>
